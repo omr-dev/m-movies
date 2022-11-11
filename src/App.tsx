68a1116c9f5c6542ec0f510f2086f312f4387a59
omer <oemer.peynirci@ontavio.de>
2022-11-11 15:39:45 +0100

    project completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 
 import './App.scss'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faCoffee} from '@fortawesome/free-solid-svg-icons';
-import Alert from 'react-bootstrap/Alert';
+// import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+// import {faCoffee} from '@fortawesome/free-solid-svg-icons';
+
+import {Container} from 'react-bootstrap';
+
+import {Movies} from './components/Movies.jsx';
+
 
 function App() {
 
 
   return (
     <div className="App">
+<Container>
 
-            <Alert key='info'  variant="info">
-                <FontAwesomeIcon icon={faCoffee}/>Lets Start!
-            </Alert>
+    <Movies />
+
+
+</Container>
 
 
     </div>
